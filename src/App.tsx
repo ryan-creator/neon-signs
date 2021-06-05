8277f64ccfc7ca92d183c665baa374869d823788
ryan <colry562@student.otago.ac.nz>
2021-06-05 12:49:59 +1200

    Create the desktop skelaton layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page">
+      <div className="button-list-container">
+        <div className="button-container">
 
+        </div>
+        <div className="button-container">
+
+        </div>
+        <div className="button-container">
+
+        </div>
+      </div>
+      <div className="sign-container">
+        <div className="sign-wrapper">
+
+        </div>
+        <div className="logo-wrapper">
+
+        </div>
+      </div>
     </div>
   );
 }
